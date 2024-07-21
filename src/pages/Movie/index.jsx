58f0bf7a6256b9e10106d3869578c9a86c105e36
febin thomas <tfebin39@gmail.com>
2024-07-21 22:12:00 +0530

    commit

--- src/pages/Movie/index.jsx
+++ src/pages/Movie/index.jsx
@@ -25,13 +25,9 @@ const Movie = () => {
   };
   useEffect(() => {
     const data = async () => {
-      const result = await fetch(
-        `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${
-          import.meta.env.VITE_API_KEY
-        }`
-      );
+      const result = await fetch(`/api/movies`);
       const jsonData = await result.json();
-      setMovieData(jsonData.results);
+      setMovieData(jsonData);
       setLoading(false);
     };
     data();
--- src/pages/TvShows/index.jsx
+++ src/pages/TvShows/index.jsx
@@ -25,13 +25,9 @@ const TvShowsPage = () => {
   };
   useEffect(() => {
     const data = async () => {
-      const result = await fetch(
-        `https://api.themoviedb.org/3/discover/tv?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${
-          import.meta.env.VITE_API_KEY
-        }`
-      );
+      const result = await fetch(`/api/series`);
       const jsonData = await result.json();
-      setMovieData(jsonData.results);
+      setMovieData(jsonData);
       setLoading(false);
     };
     data();
